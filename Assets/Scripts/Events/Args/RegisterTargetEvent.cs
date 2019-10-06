a80df37702782578821c4594c8b36f2bed68584d
Taldius <taldiusgames@gmail.com>
2019-10-06 14:52:27 +0200

    Auto stash before revert of "Merge remote-tracking branch 'origin/master' into Taldius"

---
+++ Assets/Scripts/Events/Args/RegisterTargetEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RegisterTargetEvent : EventArgs
+{
+    public RegisterTargetEvent(TargetInfo _target)
+    {
+        target = _target;
+    }
+
+    public TargetInfo target;
+}
