f7ecb94b6e5ebab59156d0b90a204d0c24ef143d
Nicolas <nicolas-donald.laurent@laposte.net>
2019-10-07 14:07:54 +0200

    Rushed dmg projectiles and life

---
+++ Assets/Scripts/Events/Args/AddTimeEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AddTimeEvent : EventArgs
+{
+    public AddTimeEvent(float _time)
+    {
+        time = _time;
+    }
+
+    public float time;
+}
---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class PlayerLife : Life
+{
+    [SerializeField] float m_damageMultiplier = 1;
+    [SerializeField] GameObject m_explosionOnTimeout = null;
+
+    SubscriberList m_subscriberList = new SubscriberList();
+
+    void Awake()
+    {
+        m_subscriberList.Add(new Event<TimeoutEvent>.Subscriber(OnTimeout));
+        m_subscriberList.Subscribe();
+    }
+
+    void OnDestroy()
+    {
+        m_subscriberList.Unsubscribe();
+    }
+
+    public override void Damage(float value)
+    {
+        Event<AddTimeEvent>.Broadcast(new AddTimeEvent(-value * m_damageMultiplier));
+    }
+
+    void OnTimeout(TimeoutEvent e)
+    {
+        var obj = Instantiate(m_explosionOnTimeout);
+        obj.transform.position = transform.position;
+
+        Destroy(gameObject);
+    }
+}
