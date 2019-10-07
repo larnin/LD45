62ca139dbae0485323c5893ce21836216741a309
Taldius <taldiusgames@gmail.com>
2019-10-07 21:35:40 +0200

    Adding twitter

---
+++ Assets/Scripts/Menu/TwitterMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitterMenu : MonoBehaviour
+{
+    [SerializeField] GameObject[] m_textArray;
+    GameObject m_currentText;
+    Animator m_animator;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        StartCoroutine(TwitterCoroutine());
+    }
+
+    IEnumerator TwitterCoroutine() {
+        yield return new WaitForSeconds(0.5f);
+
+        foreach(GameObject vText in m_textArray) {
+            if(m_currentText != null) {
+                m_currentText.SetActive(false);
+            }
+            vText.SetActive(true);
+            m_currentText = vText;
+
+            yield return new WaitForSeconds(5);
+        }
+
+        m_animator.SetTrigger("Close");
+
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+
+}
